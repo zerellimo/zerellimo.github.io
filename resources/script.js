78fe426141e959174852de5e2b8f21cef2d09986
zerellimo <12762091+zerellimo@users.noreply.github.com>
2022-10-16 17:04:57 +0200

    Implement random footer quote

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,6 @@
 
+// Dark mode
+
 const moon = document.getElementById("moon");
 
 moon.onclick = function() {
@@ -22,3 +24,24 @@ sun.onclick = function() {
     document.getElementsByClassName("dm-button")[0].style.color = "snow";
     document.getElementsByClassName("dm-button")[0].style.backgroundColor = "black";
 }
+
+// Footer quote
+
+let quotes = [
+    {
+        quote: "It has become appallingly obvious that our technology has exceeded our humanity",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "It's not a faith in technology. It's faith in people",
+        author: "Steve Jobs",
+    },
+    {
+        quote: "The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life",
+        author: "Bill Gates",
+    },
+
+];
+
+let randomQuote = Math.floor((Math.random())*quotes.length);
+document.getElementById("quote").innerHTML = [`"${quotes[randomQuote].quote}." &#8212; ${quotes[randomQuote].author}`];
