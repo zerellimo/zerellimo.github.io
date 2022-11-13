76497abb8d92e5ff7eb0b9b889eec84c9f0977f2
zerellimo <12762091+zerellimo@users.noreply.github.com>
2022-11-13 17:31:12 +0100

    Use a different quotes API

--- resources/modules/quote.js
+++ resources/modules/quote.js
@@ -4,34 +4,46 @@ export default function footerQuote() {
 
     let sampleQuotes = [
         {
-            q: "It has become appallingly obvious that our technology has exceeded our humanity.",
-            a: "Albert Einstein",
+            quote: "It has become appallingly obvious that our technology has exceeded our humanity.",
+            author: "Albert Einstein",
         },
         {
-            q: "It's not a faith in technology. It's faith in people.",
-            a: "Steve Jobs",
+            quote: "It's not a faith in technology. It's faith in people.",
+            author: "Steve Jobs",
         },
         {
-            q: "The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life.",
-            a: "Bill Gates",
+            quote: "The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life.",
+            author: "Bill Gates",
         },
     
     ];
+
+    const readOnlyAPIKey = '0AzTKB2z8fr8I8vVDtS3mQ==e2mUIilqydwiojy7';
+    const endPoint = 'https://api.api-ninjas.com/v1/quotes'
+    const requestParams = '?limit=10';
+    const urlToFetch = endPoint + requestParams;
+
+    let storedQuotes = localStorage.getItem("quotes"); 
     
     const getQuotes = async () => {
         try {
-            let response = await fetch("https://zenquotes.io/api/quotes/");
-            if (response.ok) {
-                const res = await response.json();
-                let storedQuotes = localStorage.getItem("quotes"); 
             
+            let response = await fetch(urlToFetch, {
+                method: 'GET',
+                headers: {
+                    'X-Api-Key': readOnlyAPIKey,
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (response.ok) {
+
+                const res = await response.json();
+
                 if(localStorage.getItem("quotes")!= null) {
                     display1Quote(JSON.parse(storedQuotes))
                 } else if (!res) {
                     // console.log('not resolved');
-                    display1Quote(sampleQuotes);
-                } else if (res[0].a === "zenquotes.io") {
-                    // console.log('too many requests, exceeded API limits');
                     display1Quote(sampleQuotes);
                 } else {
                     // console.log(res);
@@ -40,21 +52,19 @@ export default function footerQuote() {
                     storedQuotes = localStorage.getItem("quotes");
                     display1Quote(JSON.parse(storedQuotes));
                 }
-
             }
         } catch (error) {
             console.log(error);
+            display1Quote(sampleQuotes);
         }
 
     }
 
-    
-    
     const display1Quote = (list) => {
 
         let quoteElement = document.getElementById("quote");
         let randomNumber = Math.floor(Math.random()*list.length);
-        quoteElement.innerHTML = [`"${list[randomNumber].q}" &#8212; ${list[randomNumber].a}`];
+        quoteElement.innerHTML = [`"${list[randomNumber].quote}" &#8212; ${list[randomNumber].author}`];
         quoteElement.style.display = "inline";
         
     }
