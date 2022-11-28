99decd62f64df58a1f5636bc3800640080a9510e
zerellimo <12762091+zerellimo@users.noreply.github.com>
2022-11-28 19:17:01 +0100

    Change current nav page style

--- resources/script.js
+++ resources/script.js
@@ -12,9 +12,9 @@ sun.onclick = setLightMode;
 // footer quote
 footerQuote();
 
-// color nav element based on current page 
+// Style nav element based on current page 
 let path = window.location.pathname;
 /*let page = path.split('/').pop();
 console.log(page);*/
 let element = document.querySelector(`[href=".${path}`);
-element.style.setProperty('text-decoration', 'underline');
+element.style.setProperty('font-weight', '600');
