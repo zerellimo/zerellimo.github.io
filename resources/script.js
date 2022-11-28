a2104ae8bbd13dec40da5dc60fe81e8c68c7c330
zerellimo <12762091+zerellimo@users.noreply.github.com>
2022-11-28 19:08:46 +0100

    Style nav element based on current page

--- resources/script.js
+++ resources/script.js
@@ -11,3 +11,10 @@ sun.onclick = setLightMode;
 
 // footer quote
 footerQuote();
+
+// color nav element based on current page 
+let path = window.location.pathname;
+/*let page = path.split('/').pop();
+console.log(page);*/
+let element = document.querySelector(`[href=".${path}`);
+element.style.setProperty('text-decoration', 'underline');
